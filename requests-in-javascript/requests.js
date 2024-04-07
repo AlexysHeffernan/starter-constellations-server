4bb841681a732cac432c5bf4679ee50cfeaadd28
Alexys Heffernan <alexysheffernan@gmail.com>
2024-04-07 17:59:16 -0400

    added test

--- requests-in-javascript/requests.js
+++ requests-in-javascript/requests.js
@@ -1,13 +1,34 @@
 const axios = require("axios");
 
+
 const url = "http://localhost:5001/constellations";
 axios
   .get(url)
   .then((response) => {
-    console.log(response.status);
-    console.log(response.statusText);
-    console.log(response.data);
+    const result = response.data.filter((constellation) => {
+      return constellation.starsWithPlanets < 10;
+    });
+    console.log(result);
   })
   .catch((error) => {
     console.log(error.message);
   });
+
+axios
+  .post(url, {
+    name: "Ara",
+    meaning: "Altar",
+    starsWithPlanets: 7,
+    quadrant: "SQ3",
+  })
+  .then((response) => {
+    console.log(response.data);
+  });
+
+// Since the ID for the newly created constellation
+// is randomly generated, yours will look different from
+// what is shown here.
+const idToDelete = "JKeBEq-";
+axios.delete(`${url}/${idToDelete}`);
+
+axios.get(url);
